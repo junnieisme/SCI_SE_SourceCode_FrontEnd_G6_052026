92200ea2bc0ad105d8381026a8f6373739369aa2
DuongVu201 <duongvubs7@gmail.com>
2026-03-25 12:22:46 +0700

    ui servicespage

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Header from '../src/components/Header';
+import ServicesPage from '../src/components/ServicesPage';
+import Footer from '../src/components/Footer';
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <ServicesPage />
+      <Footer />
+    </div>
+  );
+}
